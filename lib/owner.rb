7154e3f252a3fe1a1d8fd39550efeff51cecb2a5
Tammy Metz <tammy@tammymetz.com>
2018-09-01 21:03:02 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -26,6 +26,11 @@ class Owner
     "I am a #{@species}."
   end
 
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[fishes] << new_fish
+  end
+
   def buy_cat(name)
     new_cat = Cat.new(name)
     @pets[cats] << new_cat
