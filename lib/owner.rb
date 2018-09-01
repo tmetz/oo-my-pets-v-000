10ae05132ebc14384576cae479facfcf75f854e0
Tammy Metz <tammy@tammymetz.com>
2018-09-01 21:07:09 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -46,4 +46,16 @@ class Owner
       dog.mood = "happy"
     end
   end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
 end
